b95f17a35820414afcc91e4a69745a9ba83199ab
keannu125 <noehbernasol0@gmail.com>
2023-02-28 00:44:07 +0800

    Fixed config not pointing to itself and added djoser

--- ivy/config/settings.py
+++ ivy/config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'djoser'
 ]
 
 MIDDLEWARE = [
@@ -50,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'ivy.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'ivy.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
