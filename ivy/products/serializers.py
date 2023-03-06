c486b12f33295f2d56e9836dc379adf986f87c91
keannu125 <noehbernasol0@gmail.com>
2023-03-06 15:38:47 +0800

    Add id in serializer

--- ivy/products/serializers.py
+++ ivy/products/serializers.py
@@ -5,8 +5,9 @@ from .models import Product
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
     date_added = serializers.DateTimeField(
         format="%d-%m-%Y %I:%M%p", read_only=True)
+    quantity = serializers.IntegerField(required=False, default=0)
 
     class Meta:
         model = Product
-        fields = ('name', 'quantity', 'date_added')
+        fields = ('id', 'name', 'quantity', 'date_added')
         read_only_fields = ('id', 'date_added')
