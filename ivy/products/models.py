2ea9d7a1dee6e0c9f4a8b0cbb942d15e7167d199
keannu125 <noehbernasol0@gmail.com>
2023-03-07 15:32:05 +0800

    Added history field to product model

--- ivy/products/models.py
+++ ivy/products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.timezone import now
+from simple_history.models import HistoricalRecords
 # Create your models here.
 
 
@@ -7,6 +8,7 @@ class Product(models.Model):
     name = models.CharField(max_length=20)
     quantity = models.IntegerField(default=0)
     date_added = models.DateTimeField(default=now, editable=False)
+    history = HistoricalRecords()
 
     def __str__(self):
         return self.name
